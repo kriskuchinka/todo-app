dce9d1ee72be1fdf88cedfc77d5a861280b3057b
Kris Kuchinka <kriskuchinka@gmail.com>
2023-06-28 21:33:13 -0700

    Additions to catch up with Victor.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 function Header() {
+  return (
+    <>
+      <h1 className="bg-primary">From the Desk of Kris Kuchinka</h1>
+    </>
+  );
+}
 
-
-    return (
-      <>
-        <h1>From the Desk of Kris Kuchinka</h1>
-      </>
-    )
-  }
-  
-  export default Header
+export default Header;
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,20 +1,48 @@
-import CheckListItem from './CheckListItem'
+import { useState } from "react";
+import toDoData from "../../toDoData";
+import CheckListItem from "./CheckListItem";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 function MainContent() {
-    const checkListItem= [{toDoValue: "Groceries", toDoItem: "Buy Groceries"}, {toDoValue: "Dishes", toDoItem: "Clean"}, {toDoValue: "Code", toDoItem: "Code"}, {toDoValue: "Bowl", toDoItem: "Go Bowling"}];
+  const [toDoState, setToDoState] = useState(toDoData);
 
-    const mappedCheckList = checkListItem.map(item=><CheckListItem key={item.id} toDoValue={item.toDoValue} toDoItem={item.toDoItem} />);
+  const handleCheckBoxClick = (toDoKey) => {
+    const newToDoList = toDoState.map((item) => {
+      if (item.id === toDoKey) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+    setToDoState(newToDoList);
+  };
 
-    return (
-      <>
-        <section className="paper">
-            <h1>Things I Need To Do:</h1>
-            {mappedCheckList}
-        </section>  
+  const mappedCheckList = toDoState.map((item) => (
+    // <CheckListItem
+    //   toDoKey={item.id}
+    //   toDoValue={item.text}
+    //   toDoItem={item.completed}
+    //   handleCheckBoxClick={handleCheckBoxClick}
+    // />
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={item.completed}
+          onChange={() => handleCheckBoxClick(item.id)}
+        />
+      }
+      label={item.text}
+    />
+  ));
 
-      </>
-    )
-  }
-  
-export default MainContent
-  
+  return (
+    <>
+      <section className="paper">
+        <h1>Things I Need To Do:</h1>
+        {mappedCheckList}
+      </section>
+    </>
+  );
+}
+
+export default MainContent;
