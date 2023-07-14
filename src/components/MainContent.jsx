cf03a08ca6e3d5c22221b09cea3aa113a6c45738
Kris Kuchinka <kriskuchinka@gmail.com>
2023-07-13 17:12:01 -0700

    classtime additions I forgot to upload.

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -10,36 +10,63 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 function MainContent() {
-  const [toDoState, setToDoState] = useState(toDoData);
+  const [toDoState, setToDoState] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [isPencilClicked, setIsPencilClicked] = useState(false);
   const [toDoId, setToDoId] = useState(null);
+  const [editInputValue, setEditInputValue] = useState("");
 
-  const addItem = () => {
-    const tempArray = [
-      ...toDoState,
-      {
-        id: toDoState.length + 1,
-        text: inputValue,
-        completed: false,
-      },
-    ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:5000/toDos");
+      // console.log(response);
+      const data = await response.json();
+      setToDoState(data.toDos);
+    };
+
+    fetchData();
+  }, [toDoState]);
+
+  const addItem = async () => {
+    setInputValue("");
+    const newItem = {
+      id: toDoState.length + 1,
+      text: inputValue,
+      completed: false,
+    };
+    const tempArray = [...toDoState, newItem];
+
+    const response = await fetch("http://localhost:5000/newitem", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newItem),
+    });
+
+    if (!response.ok) {
+      console.error("Error while adding item.", response.statusText);
+    }
     console.log(tempArray);
 
     setToDoState(tempArray);
   };
 
-  const updateItem = (e, id) => {
-    let tempArray = [...toDoState];
-    const updatedToDoList = tempArray.map((item) => {
-      if (item.id === id) {
-        item.text = inputValue;
-      }
-      return item;
-    });
-    console.log(updatedToDoList);
+  const updateItem = async (e, id) => {
+    const itemToUpdate = toDoState.find((item) => item.id === id);
+    itemToUpdate.text = editInputValue;
+    console.log(itemToUpdate, "item to update");
     setIsPencilClicked(!isPencilClicked);
-    setToDoState(updatedToDoList);
+    const response = await fetch(`http://localhost:5000/edititem/${id}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(itemToUpdate),
+    });
+    if (!response.ok) {
+      console.error("Error while updating item.", response.statusText);
+    }
+    let tempArray = toDoState.map((item) =>
+      item.id === id ? itemToUpdate : item
+    );
+    setToDoState(tempArray);
   };
 
   const handleCheckBoxClick = (toDoKey) => {
@@ -53,15 +80,21 @@ function MainContent() {
   };
 
   const handlePencilClick = (id, text) => {
-    setInputValue(text);
+    setEditInputValue(text);
     setIsPencilClicked(true);
     setToDoId(id);
   };
 
-  const handleRemoveToDoItem = (id) => {
+  const handleRemoveToDoItem = async (id) => {
     let tempArray = [...toDoState];
     const updatedList = tempArray.filter((item) => item.id !== id);
     setToDoState(updatedList);
+    const response = await fetch(`http://localhost:5000/deleteitem/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      console.error("Error while deleting item.", response.statusText);
+    }
   };
 
   const mappedCheckList = toDoState.map((item) => (
@@ -76,9 +109,8 @@ function MainContent() {
         <>
           <Stack spacing={2} direction="row">
             <Input
-              onChange={(e) => setInputValue(e.target.value)}
-              placeholder="Add Item"
-              value={inputValue}
+              onChange={(e) => setEditInputValue(e.target.value)}
+              value={editInputValue}
             />
             <Button onClick={(e) => updateItem(e, item.id)} variant="contained">
               Update
@@ -130,8 +162,9 @@ function MainContent() {
           <Input
             onChange={(e) => setInputValue(e.target.value)}
             placeholder="Add Item"
+            value={inputValue}
           />
-          <Button onClick={addItem} variant="contained">
+          <Button onClick={() => addItem()} variant="contained">
             Add Item
           </Button>
         </Stack>
